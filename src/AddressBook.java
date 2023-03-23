7a9e8243c03f21db0cba61a6c4912e5212a23d60
debabrata-97 <debabrataszr@gmail.com>
2023-03-23 18:03:59 +0530

    Added : Code to Edit Contacts in Addressbook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,46 +1,110 @@
 import java.util.Scanner;
 
 public class AddressBook {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of persons to add : ");
-        int persons = sc.nextInt();
-        Contacts arr[] = new Contacts[persons];
-        for (int i = 0; i < persons; i++) {
-            System.out.println("Firstname :");
-            String firstname = sc.next();
-            System.out.println("Lastname : ");
-            String lastname = sc.next();
-            System.out.println("Address : ");
-            String address = sc.next();
-            System.out.println("City : ");
-            String city = sc.next();
-            System.out.println("State : ");
-            String state = sc.next();
-            System.out.println("Zipcode : ");
-            int zip = sc.nextInt();
-            System.out.println("Phone No. : ");
-            long phoneno = sc.nextLong();
-            System.out.println("Email : ");
-            String email = sc.next();
+        askUser();
+    }
 
-            Contacts contact = new Contacts();
-            contact.firstname = firstname;
-            contact.lastname = lastname;
-            contact.address = address;
-            contact.city = city;
-            contact.state = state;
-            contact.zip = zip;
-            contact.phoneno = phoneno;
-            contact.email = email;
-            arr[i] = contact;
+    public static void askUser() {
+        System.out.println("Select an option..");
+        System.out.println("1. Add a person");
+        System.out.println("2. Edit");
+        System.out.println("3. Quit");
+
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                addAPerson();
+                System.out.println("\n");
+                askUser();
+                break;
+            case 2:
+                edit();
+                System.out.println("\n");
+                askUser();
+                break;
+            default:
+                break;
         }
+    }
 
-        for (int i = 0; i < persons; i++) {
-            System.out.println(arr[i].firstname + " " + arr[i].lastname + " " + arr[i].address + " " + arr[i].city + " "
-                    + arr[i].state + " " + arr[i].zip + " " + arr[i].phoneno + " " + arr[i].email);
+    public static void addAPerson() {
+        System.out.println("Firstname : ");
+        String firstname = sc.next();
+        System.out.println("Lastname : ");
+        String lastname = sc.next();
+        System.out.println("Address : ");
+        String address = sc.next();
+        System.out.println("City : ");
+        String city = sc.next();
+        System.out.println("State : ");
+        String state = sc.next();
+        System.out.println("Zipcode : ");
+        int zip = sc.nextInt();
+        System.out.println("Phone No. : ");
+        long phoneno = sc.nextLong();
+        System.out.println("Email : ");
+        String email = sc.next();
+
+        Contacts contact = new Contacts();
+        contact.firstname = firstname;
+        contact.lastname = lastname;
+        contact.address = address;
+        contact.city = city;
+        contact.state = state;
+        contact.zip = zip;
+        contact.phoneno = phoneno;
+        contact.email = email;
+    }
+
+    public static void edit() {
+        System.out.println("What do you want to edit?");
+        System.out.println("1. First name ");
+        System.out.println("2. Last name ");
+        System.out.println("3. Address ");
+        System.out.println("4. City ");
+        System.out.println("5. State ");
+        System.out.println("6. Zip ");
+        System.out.println("7. Phone No. ");
+        System.out.println("8. Email ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Firstname : ");
+                String firstname = sc.next();
+                break;
+            case 2:
+                System.out.println("Lastname : ");
+                String lastname = sc.next();
+                break;
+            case 3:
+                System.out.println("Address : ");
+                String address = sc.next();
+                break;
+            case 4:
+                System.out.println("City : ");
+                String city = sc.next();
+                break;
+            case 5:
+                System.out.println("State : ");
+                String state = sc.next();
+                break;
+            case 6:
+                System.out.println("Zipcode : ");
+                int zip = sc.nextInt();
+                break;
+            case 7:
+                System.out.println("Phone No. : ");
+                long phoneno = sc.nextLong();
+                break;
+            case 8:
+                System.out.println("Email : ");
+                String email = sc.next();
+                break;
         }
-
+        System.out.println("Contact edited successfully");
     }
 }
--- src/Contacts.java
+++ src/Contacts.java
@@ -7,21 +7,70 @@ public class Contacts {
     int zip;
     long phoneno;
     String email;
-    public Contacts() {
 
+    public String getFirstname() {
+        return firstname;
     }
-    public Contacts(String firstname, String lastname, String address,
-                   String state, int zip, int phoneno, String email) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.address = address;
-        this.city=city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneno = phoneno;
-        this.email = email;
 
-}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(long phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     @Override
     public String toString() {
