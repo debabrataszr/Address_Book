1b62c5371ce13640d354934e04621cd32f7b8f6f
debabrata-97 <debabrataszr@gmail.com>
2023-03-24 20:25:39 +0530

    Added : Code to View Persons by city or state

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AddressBookMain {
@@ -18,9 +15,10 @@ public class AddressBookMain {
         while (loop) {
             System.out.println("Enter what you want to perform : ");
             System.out.println("Press 1 to create new address book" + '\n' + "Press 2 to perform operation " +
-                    "on existing address book" + '\n' + "Press 3 to search contacts with city " + '\n' + "Press 0 to exit");
-            final int createAddressBook = 1, operateExisting = 2, searchContacts = 3, exit = 0;
-            int option=sc.nextInt();
+                    "on existing address book" + '\n' + "Press 3 to search contacts with city " + '\n'
+                    + "Press 4 to get person with city" + '\n' + "Press 0 to exit");
+            final int createAddressBook = 1, operateExisting = 2, searchContacts = 3,getPersonWithCity = 4, exit = 0;
+            int option = sc.nextInt();
             switch (option) {
                 case createAddressBook:
                     abm.createAddressBook();
@@ -33,8 +31,11 @@ public class AddressBookMain {
                     else
                         System.out.println("Entered key address book not available");
                     break;
-                case  searchContacts:
+                case searchContacts:
                     abm.searchContactsWithCity();
+                    break;
+                case getPersonWithCity:
+                    abm.getContactByCityAndState();
                     break;
                 case exit:
                     ab.toQuit();
@@ -64,8 +65,15 @@ public class AddressBookMain {
         List<Contacts> listOfContacts = addressbook.values().stream().flatMap(p -> p.stream()).filter(p -> p.getCity().equals(cityName)).collect(Collectors.toList());
         for (Contacts contact : listOfContacts) {
             if (contact.getCity().equals(cityName)) {
-                System.out.println(contact);
+                System.out.println(listOfContacts);
             }
         }
     }
-}
+            public void getContactByCityAndState() {
+                List<Contacts> myContactList = addressbook.values().stream().flatMap(A -> A.stream()).collect(Collectors.toList());
+                Map<String, List<Contacts>> myContactListByCity = myContactList.stream().collect(Collectors.groupingBy(Contacts::getCity));
+                System.out.println(myContactListByCity);
+                Map<String, List<Contacts>> myContactListByState = myContactList.stream().collect(Collectors.groupingBy(Contacts::getState));
+                System.out.println(myContactListByState);
+            }
+        }
